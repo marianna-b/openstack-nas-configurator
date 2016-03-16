efe3ae3fdc3a9fc5007fc2cc3fbd45f4bd1288f7
Marianna <bisyarinamariashka@gmail.com>
2016-03-16 13:37:00 +0300

    Added allocation and reservation of ip address in allocation pools of subnet

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ def check(ip, subn):
 		ipset.add(IPRange(p['start'], p['end']))
 	if not(ip in ipset):
 		return False
+
 	ports = neutron.list_ports()['ports']
 	for p in ports:
 		for i in p['fixed_ips']:
@@ -57,20 +58,65 @@ def check(ip, subn):
 					return False
 	return True
 
+def alloc_ip(subn):
+	pools = subn['allocation_pools']	
+	ans = ""	
+	for p in pools:
+		range = IPRange(p['start'], p['end'])
+		for ip in range:
+			if check(ip, subn):
+				ans = ip
+	reserv_ip(ans, subn)	
+
+def reserv_ip(ip, subn):
+	pools = subn['allocation_pools']	
+	new_pools = []
+	ip_addr = IPAddress(ip)
+	for p in pools:
+		if ip in IPRange(p['start'], p['end']):
+			if p['start'] == p['end']:
+				continue
+			if ip != p['end']:
+				new_pools.append(
+						{'start': str(ip_addr + 1),
+					 	 'end' : p['end'] })	
+			if ip != p['start']:
+				new_pools.append(
+						{'start': p['start'],
+						 'end' :  str(ip_addr - 1)})	
+		else:
+			new_pools.append(p)
+	
+	subn2 = {'allocation_pools' : new_pools }
+	try:
+		neutron.update_subnet(subn['id'], {'subnet' : subn2})
+	except Exception, e:
+		print str(e)
+
 def join():
 	if len(sys.argv) < 4:
 		print "Not enough arguments for join" #TODO add help
 		return
 	subn = sys.argv[2]
 	serv = sys.argv[3]
+	res = True
+	cur_ip = ""
+	subnet = neutron.list_subnets(name=subn)['subnets'][0]
 	if serv_ip.has_key(serv):
 		try:
-			res = check(serv_ip.get(serv), neutron.list_subnets(name=subn)['subnets'][0])
+			cur_ip = serv_ip.get(serv)
+			res = check(cur_ip, subnet)
+			if res:
+				reserv_ip(cur_ip, subnet)
 		except:
 			print "Unknown subnet"
 			return		
-	
-
+	else:
+		cur_ip = alloc_ip(subnet)
+	if not res:
+		print "Failed to start server: bad ip address"
+		#TODO
+		return		
 
 
 parse = {"subnet-list" : subnet_list,
